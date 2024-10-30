7defb8a720fe5873f1d006e16ed50b4b0f419996
Federico Luna <federicolun4@gmail.com>
2024-10-30 03:48:47 -0300

    agrego todas las rutas

--- app.py
+++ app.py
@@ -2,9 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def asd():
-    return render_template('index.html')
 
-if __name__ == '__main__':
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/publicar")
+def publicar():
+    return render_template("publicar.html")
+
+
+@app.route("/favoritos")
+def favoritos():
+    return render_template("favoritos.html")
+
+
+@app.route("/buscar")
+def buscar():
+    return render_template("buscar.html")
+
+
+@app.route("/iniciar-sesion")
+def login():
+    return render_template("iniciar-sesion.html")
+
+
+@app.route("/registrarse")
+def registrarse():
+    return render_template("registrarse.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
