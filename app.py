abf26fd0077107e324c5cd5ee36f99614995f445
Federico Luna <federicolun4@gmail.com>
2024-11-13 23:03:52 -0300

    ultimos arreglos

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ import os
 import datetime
 from flask import Flask, render_template, request, flash, url_for, redirect, session
 from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
 from werkzeug.utils import secure_filename
 from dotenv import load_dotenv
 from flask_bcrypt import Bcrypt
-from werkzeug.security import generate_password_hash, check_password_hash
 
 load_dotenv()  # Cargar las variables de entorno desde el archivo .env
 
@@ -21,16 +19,16 @@ app.config["SQLALCHEMY_DATABASE_URI"] = (
     f"mysql+pymysql://{user}:{password}@{host}/{db_name}"
 )
 
-app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+pymysql://{user}:{password}@{host}/{db_name}"
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    f"mysql+pymysql://{user}:{password}@{host}/{db_name}"
+)
 app.config["UPLOAD_FOLDER"] = "static/posts"
 app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024  # 16 MB
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.secret_key = os.getenv('SECRET_KEY')
+app.secret_key = os.getenv("SECRET_KEY")
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-ma = Marshmallow(app)
-
 
 
 class User(db.Model):
@@ -39,12 +37,13 @@ class User(db.Model):
     apellido = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(1000), nullable=False)
-    
+
     def __init__(self, username, email, password, apellido):
         self.username = username
         self.email = email
         self.password = password
         self.apellido = apellido
+
 
 class MensajeForm(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -56,7 +55,7 @@ class MensajeForm(db.Model):
         self.nombre = nombre
         self.email = email
         self.mensaje = mensaje
-    
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -103,10 +102,12 @@ class Post(db.Model):
         self.direccion = direccion
         self.fecha_publicacion = fecha_publicacion
         self.esFavorito = esFavorito
-        
+
+
 # Crear la base de datos y las tablas
 with app.app_context():
     db.create_all()
+
 
 @app.route("/publicar", methods=["GET", "POST"])
 def publicar():
@@ -151,6 +152,7 @@ def publicar():
 
     return render_template("publicar.html")
 
+
 @app.route("/enviar_mensaje", methods=["POST"])
 def enviar_mensaje():
     nombre = request.form["nombre"]
@@ -164,6 +166,7 @@ def enviar_mensaje():
     flash("Mensaje enviado exitosamente", "success")
     return redirect(url_for("index"))
 
+
 @app.route("/favoritos")
 def favoritos():
     favoritos = Post.query.filter_by(
@@ -171,19 +174,20 @@ def favoritos():
     ).all()  # Filtrar solo los posts favoritos
     return render_template("favoritos.html", posts=favoritos)
 
+
 @app.route("/toggle_favorite/<int:post_id>", methods=["POST"])
 def toggle_favorite(post_id):
     post = Post.query.get_or_404(post_id)
     post.esFavorito = not post.esFavorito  # Alternar el estado de esFavorito
     db.session.commit()
-    return '', 204  # Responder con un estado 204 No Content
+    return "", 204  # Responder con un estado 204 No Content
+
 
 @app.route("/buscar")
 def buscar():
     postList = Post.query.all()
     return render_template("buscar.html", posts=postList)
 
-# TODO: Crear las rutas de detalle de cada post. Con metodo delete y get
 
 @app.route("/post/<int:id>", methods=["GET", "DELETE"])
 def post_detail(id):
@@ -196,21 +200,25 @@ def post_detail(id):
         post = Post.query.get_or_404(id)
         return render_template("post.html", post=post)
 
+
 @app.route("/iniciar-sesion", methods=["GET", "POST"])
 def iniciar_sesion():
     if request.method == "POST":
         email = request.form["email"]
         password = request.form["password"]
         user = User.query.filter_by(email=email).first()
-        
+
         if user and bcrypt.check_password_hash(user.password, password):
             session["user_id"] = user.id
             flash("Inicio de sesión exitoso", "success")
-            return redirect(url_for("index", welcome_message=f"Bienvenido, {user.username}!"))
+            return redirect(
+                url_for("index", welcome_message=f"Bienvenido, {user.username}!")
+            )
         else:
             flash("Usuario o contraseña incorrecta", "danger")
-    
+
     return render_template("iniciar_sesion.html")
+
 
 @app.route("/registrar", methods=["GET", "POST"])
 def registrar():
@@ -226,21 +234,32 @@ def registrar():
             return redirect(url_for("registrar"))
 
         # Verificar si el nombre de usuario o el correo electrónico ya existen
-        user = User.query.filter((User.username == username) | (User.email == email)).first()
+        user = User.query.filter(
+            (User.username == username) | (User.email == email)
+        ).first()
         if user:
-            flash("El nombre de usuario o el correo electrónico ya están en uso", "danger")
+            flash(
+                "El nombre de usuario o el correo electrónico ya están en uso", "danger"
+            )
             return redirect(url_for("registrar"))
 
-        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-        
-        new_user = User(username=username, apellido=apellido, email=email, password=hashed_password)
+        hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
+
+        new_user = User(
+            username=username, apellido=apellido, email=email, password=hashed_password
+        )
         db.session.add(new_user)
         db.session.commit()
-        
+
         flash("Usuario registrado correctamente", "success")
-        return redirect(url_for("index", welcome_message=f"Usuario registrado exitosamente, {username}!"))
-    
+        return redirect(
+            url_for(
+                "index", welcome_message=f"Usuario registrado exitosamente, {username}!"
+            )
+        )
+
     return render_template("registrarse.html")
+
 
 @app.route("/")
 def index():
